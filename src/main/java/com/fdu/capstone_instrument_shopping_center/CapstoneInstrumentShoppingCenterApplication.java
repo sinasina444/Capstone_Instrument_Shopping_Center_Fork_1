1c8f69d33755383b8d5c093fbb5cd5f7af91d909
Xiao Su <stansu743@gmail.com>
2024-09-16 19:52:08 -0700

    first commit

---
+++ src/main/java/com/fdu/capstone_instrument_shopping_center/CapstoneInstrumentShoppingCenterApplication.java
@@ -0,0 +1,13 @@
+package com.fdu.capstone_instrument_shopping_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneInstrumentShoppingCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneInstrumentShoppingCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fdu/capstone_instrument_shopping_center/CapstoneInstrumentShoppingCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdu.capstone_instrument_shopping_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneInstrumentShoppingCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
